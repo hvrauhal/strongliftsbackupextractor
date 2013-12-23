5decc579e39b94a460eec93a64cee3c7ccb2ce88
Heikki Rauhala <heikki.rauhala@reaktor.fi>
2013-12-23 11:08:48 +0200

    Use the printer itself to print sample

--- public/index.js
+++ public/index.js
@@ -8,11 +8,14 @@ $(function() {
       cache: false,
       contentType: false,
       processData: false
-    }).done(function (workouts) {
-        $('#workouts .results').empty().append($('<ul>').append(workouts.map(printSession)))
-      }
-    );
+    }).done(displayWorkouts('#workouts .results'))
   })
+  function displayWorkouts(target) {
+    return function (workouts) {
+      console.log('First three', JSON.stringify(_.first(workouts, 3)))
+      $(target).empty().append($('<ul>').append(workouts.map(printSession)))
+    }
+  }
 
   function printSession(session) {
     return $('<li>')
@@ -42,4 +45,7 @@ $(function() {
     .ajaxStop(function () {
       $('#workouts .loading').hide()
     })
+
+  var samples = [{"date":"2013-09-14T11:10:15Z","n":1,"notes":"Felt good!","bodyweight":76,"exercises":[{"n":0,"weight":20,"name":"squats","sets":[5,5,5,5,5],"totalweight":0},{"n":1,"weight":20,"name":"benchpress","sets":[5,5,5,5,5],"totalweight":0},{"n":2,"weight":30,"name":"barbellrow","sets":[5,5,5,5,5],"totalweight":0}]},{"date":"2013-09-16T05:03:12Z","n":2,"notes":"The shoulderpress felt quite heavy with just the bar","bodyweight":76,"exercises":[{"n":0,"weight":22.5,"name":"squats","sets":[5,5,5,5,5],"totalweight":0},{"n":1,"weight":20,"name":"overheadpress","sets":[5,5,5,5,5],"totalweight":0},{"n":2,"weight":40,"name":"deadlifts","sets":[5,null,0,0,0],"totalweight":0}]},{"date":"2013-09-18T05:21:10Z","n":3,"notes":"The Bench press feels good","bodyweight":76,"exercises":[{"n":0,"weight":25,"name":"squats","sets":[5,5,5,5,5],"totalweight":0},{"n":1,"weight":22.5,"name":"benchpress","sets":[5,5,5,5,5],"totalweight":0},{"n":2,"weight":32.5,"name":"barbellrow","sets":[5,5,5,5,5],"totalweight":0}]}]
+  displayWorkouts('#workouts .example .exampleworkout')(samples)
 })
--- web.js
+++ web.js
@@ -12,6 +12,7 @@ app.configure(function () {
   app.use(logfmt.requestLogger())
   app.use(app.router)
   app.use(less({ src: __dirname + '/public', compress:false }))
+  app.get('/vendor/lodash.min.js', function(req, res) { res.sendfile('/node_modules/lodash/dist/lodash.min.js', { root: __dirname + '/' })})
   app.use(express.static(path.join(__dirname, 'public')))
 })
 
